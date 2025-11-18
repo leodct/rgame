b58b84b7cc41f0d1f6ee0a51dd137314794a1323
Leo dct <leo555.ldct@gmail.com>
2025-11-18 12:40:01 +0100

    Added documentation to the GameObject class, cleaned it up a bit and removed rock.hpp which had somehow managed to sneak all the way over here :skull:

--- include/gameObject.hxx
+++ include/gameObject.hxx
@@ -3,19 +3,42 @@
 
 #include "transform.hxx"
 
+/**
+ * @brief ## Game Object parent class
+ * @brief Pure virtual class to provide generic methods to its child classes.
+ */
 class GameObject{
 protected:
-    Transform2D transform;
+    Transform2D transform; // Position, scale and rotation data.
 public:
+    /**
+     * @brief Create a new `GameObject` object with default parameters.
+     * @param _transform New object's `transform` data. Defines position, rotation and scale.
+     */
     GameObject(Transform2D _transform = {});
-    GameObject(Vector2 position);
-    GameObject(Vector2 position, float scale);
+    /**
+     * @brief Default virtual destructor.
+     */
     virtual ~GameObject() = default;
 
+    /**
+     * @brief Get a @b constant reference to the object's `transform` data.
+     * @return A @b constant reference to the object's `transform` data.
+     */
     const Transform2D &GetTransform() const;
+    /**
+     * @brief Get a reference to the object's `transform` data.
+     * @return A @b non-constant reference to the object's `transform` data.
+     */
     Transform2D       &GetTransform();
 
+    /**
+     * @brief Pure virtual function to be overriden in child classes.
+     */
     virtual void Draw() const = 0;
+    /**
+     * @brief Pure virtual function to be overriden in child classes.
+     */
     virtual void Update()     = 0;
 };
 #endif
--- src/gameObject.cpp
+++ src/gameObject.cpp
@@ -4,17 +4,6 @@ GameObject::GameObject(Transform2D _transform) : transform(_transform)
 {
 }
 
-GameObject::GameObject(Vector2 position)
-{
-    transform.position = position;
-}
-
-GameObject::GameObject(Vector2 position, float scale)
-{
-    transform.position = position;
-    transform.scale    = scale;
-}
-
 Transform2D &GameObject::GetTransform()
 {
     return transform;
